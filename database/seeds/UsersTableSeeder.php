ff1fa2ea6e52f18be28c6f4f0f3719741d24a8cc
Fabio Pereira de Lima <fabio.fapeli@gmail.com>
2016-12-22 11:21:19 -0200

    route login to markers instead home

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,6 +15,7 @@ class UsersTableSeeder extends Seeder
     {
         //DB::statement("SET foreign_key_checks = 0");
         //User::truncate();
+        DB::statement("TRUNCATE TABLE users CASCADE");
         User::create(['name'=>'Cristiano','email'=>'cristiano@cristianolamas.com.br','password'=>bcrypt('123456'),'is_admin'=>1]);
         User::create(['name'=>'Fábio','email'=>'fabio.fapeli@gmail.com','password'=>bcrypt('123456'),'is_admin'=>0]);
         App::truncate();
