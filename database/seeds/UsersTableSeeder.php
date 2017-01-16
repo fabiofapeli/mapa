dfbb082ad7204267545923a36bef08e8d5930b93
Fabio Pereira de Lima <fabio.fapeli@gmail.com>
2017-01-16 11:15:02 -0300

    Ajuste

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,8 +15,8 @@ class UsersTableSeeder extends Seeder
         //DB::statement("SET foreign_key_checks = 0");
         //User::truncate();
         DB::statement("TRUNCATE TABLE users CASCADE");
-        User::create(['name'=>'Cristiano','email'=>'cristiano@cristianolamas.com.br','password'=>bcrypt('123456'),'is_admin'=>1]);
-        User::create(['name'=>'Fábio','email'=>'fabio.fapeli@gmail.com','password'=>bcrypt('123456'),'is_admin'=>0]);
+        User::create(['id'=>1,'name'=>'Cristiano','email'=>'cristiano@cristianolamas.com.br','password'=>bcrypt('123456'),'is_admin'=>1]);
+        User::create(['id'=>2,'email'=>'fabio.fapeli@gmail.com','password'=>bcrypt('123456'),'is_admin'=>0]);
        
     }
 }
