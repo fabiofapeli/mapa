5dae1f50aafd26cf4a64f17f9492b965a52fc76b
Fabio Pereira de Lima <fabio.fapeli@gmail.com>
2017-01-16 11:18:19 -0300

    Ajuste

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
         //User::truncate();
         DB::statement("TRUNCATE TABLE users CASCADE");
         User::create(['id'=>1,'name'=>'Cristiano','email'=>'cristiano@cristianolamas.com.br','password'=>bcrypt('123456'),'is_admin'=>1]);
-        User::create(['id'=>2,'email'=>'fabio.fapeli@gmail.com','password'=>bcrypt('123456'),'is_admin'=>0]);
+        User::create(['id'=>2,'name'=>'Fabio','email'=>'fabio.fapeli@gmail.com','password'=>bcrypt('123456'),'is_admin'=>0]);
        
     }
 }
